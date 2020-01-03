bffb524deb8e9b7ff7e01585a7d218827f264555
Larhwally <lawalwhylawal@gmail.com>
2020-01-04 00:27:59 +0100

    init test

---
+++ source/repos/Unilag_Medical_Center/AutoMedic/DTO/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMedic.DTO
+{
+    public class UserDTO
+    {
+        public int ItbId { get; set; }
+        public string Username { get; set; }
+        public string User_type { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ source/repos/Unilag_Medical_Center/AutoMedic/Model/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMedic.Model
+{
+    public class Users
+    {
+        public int ItbId { get; set; }
+        public string  Username { get; set; }
+        public string User_type { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
